e1f47c8c6bc2ced016dd180fd3257b20e0206185
Tomáš Diblík <dibla.tomas@post.cz>
2023-07-10 06:24:17 +0200

    [Init] UI Project structure

---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<!-- 
+  1. Check if there are any user accounts.
+  2. Check if the refresh token is valid and if it isn't remove the user account and force a re-login
+-->
+
+<nav>
+  <a class="link" href="/">home</a>
+  <a class="link" href="/about">about</a>
+</nav>
+
+<h1 class="text-3xl text-purple-500 font-bold underline">Home</h1>
+<p>this is the home page.</p>
+
+<button class="btn">Button</button>
+<button class="btn btn-neutral">Neutral</button>
+<button class="btn btn-primary">Button</button>
+<button class="btn btn-secondary">Button</button>
+<button class="btn btn-accent">Button</button>
+<button class="btn btn-ghost">Button</button>
+<button class="btn btn-link">Button</button>
---
+++ ui/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["night"],
+    darkTheme: "night",
+    base: true,
+    styled: true,
+    utils: true,
+    rtl: false,
+    prefix: "",
+    logs: true,
+  },
+};
